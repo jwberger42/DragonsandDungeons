ceaa539dcc025bbbda08a60eed36bd174a72be36
jwberger42 <jwberger@shaw.ca>
2017-02-08 09:51:50 -0700

    Added basic fucntional GUI

--- src/com/dnd/Main.java
+++ src/com/dnd/Main.java
@@ -1,8 +1,23 @@
 package com.dnd;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-	System.out.println("Git Test of IntelliJ Idea!");
-    }
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void start(Stage dndStage) throws Exception {
+		// TODO Auto-generated method stub
+		dndStage.setTitle("DND");
+		FlowPane rootNode = new FlowPane(10,10);
+		Scene dndScene = new Scene(rootNode, 1000, 1000);
+		dndStage.setScene(dndScene);
+		dndStage.show();
+	}
+
 }
