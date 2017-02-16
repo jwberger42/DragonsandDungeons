53f00c82f3d10056698efe7cce7bf0a59fd2ebc2
TheIndianITGuy <jazzberryvader@gmail.com>
2017-02-16 09:16:46 -0700

    Update itemlocation.java

--- src/com/dnd/itemlocation.java
+++ src/com/dnd/itemlocation.java
@@ -1,1 +1,13 @@
-
+package.com.dnd;
+//try to program array whatever the hell that means, learn about it pretty soon.
+public class Location
+{
+  //world x values
+  private static int northWorldedge = 5;
+  private static int southWorldedge = -5;
+  //world y values
+  private static int eastWorldedge = 5;
+  private static int westWorldedge = -5;
+  private static int player
+  
+}
