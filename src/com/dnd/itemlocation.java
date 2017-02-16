958b0a93a3c28f50a13701f3e8b9b5d9ccbc3c9c
TheIndianITGuy <jazzberryvader@gmail.com>
2017-02-16 09:52:08 -0700

    Update itemlocation.java

--- src/com/dnd/itemlocation.java
+++ src/com/dnd/itemlocation.java
@@ -45,50 +45,5 @@ public class Location
     }
     loop();
   }
-  package com.dnd;
 
-import java.util.Scanner;
-
-//try to program array whatever the hell that means, learn about it pretty soon.
-public class Location
-{
-  //world y values
-  private static int northWorldedge = 5;
-  private static int southWorldedge = -5;
-  //world x values
-  private static int eastWorldedge = 5;
-  private static int westWorldedge = -5;
-  //player world location
-  private static int playerX = 0;
-  private static int playerY = 0;
-  //grappling hook location
-  private static int hookX = 3;
-  private static int hookY = 3;
-  //Scanner code
-  public static Scanner userin = new Scanner(System.in);
-  
-  public Location()
-  {    
-  }
-  
-  public static void loop()
-  {
-    doStuff();
-  }
-  
-  public static void main(String[] args)
-  {
-    loop();
-  }
-  
-  public static void doStuff()
-  {
-    String test = userin.next();
-    if (test.contains("forward"))
-    {
-      playerY = playerY + 1;
-      System.out.println(playerY);
-    }
-    loop();
-  }
   
