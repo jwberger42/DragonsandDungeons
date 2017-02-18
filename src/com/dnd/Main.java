6bb39bdea8bbe1ccc55f9ede89ff7de14cd94f7d
jwberger42 <jwberger@shaw.ca>
2017-02-18 14:43:09 -0700

    starting to upgrade the basic framework for a map
    Signed-off-by: jwberger42 <jwberger@shaw.ca>

--- src/com/dnd/Main.java
+++ src/com/dnd/Main.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
@@ -28,7 +31,10 @@ public class Main extends Application {
     //static ArrayList<String> loadedNpcs = new ArrayList<>();
     private static ObservableList<String> allItems;
     private static Image map;
-    private static ImageView mapView;    private static File mapImg;
+    private static ImageView mapView;
+    private static File mapImg;
+    public static TextField input;
+    public static ArrayList<String> output;
 
     static {
         allItems = FXCollections.observableArrayList();
@@ -75,7 +81,14 @@ public class Main extends Application {
         mapView.setImage(map);
         mapView.setSmooth(true);
         mapView.setCache(true);
-        rootNode.getChildren().addAll(/*label, availItems*/mapView);
-
+        input = new TextField();
+        input.setPromptText("Enter your input here");
+        input.setEditable(true);
+        output = new ArrayList<String>();
+        rootNode.getChildren().addAll(/*label, availItems*/mapView,input,output);
+        input.setOnAction(event -> {
+            output.add(input.getText());
+            input.selectAll();
+        });
     }
 }
